b3d3adfd2cdaf4ff94931b2e5322ebf60db081cc
TreesWise <140375595+TreesWise@users.noreply.github.com>
2025-02-27 13:01:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,3 @@
-# from fastapi import FastAPI, File, UploadFile, HTTPException
-# from fastapi.responses import JSONResponse
-# import os
-# import shutil
-# from cv_json_gemini import cv_json
-# import json
- 
-# app = FastAPI()
-
-
-# @app.post("/upload/")
-# async def upload_file(file: UploadFile = File(...)):
-#     try:
-#         filename = file.filename.lower()
-#         if not filename.endswith((".pdf", ".doc", ".docx")):
-#             raise HTTPException(status_code=400, detail="Only PDF and Word documents are allowed")
-
-#         file_path = os.path.join(file.filename)
-#         with open(file_path, "wb") as buffer:
-#             shutil.copyfileobj(file.file, buffer)
-        
-#         extracted_json = await cv_json(file_path)
-#         print(extracted_json)
-
-#         # if not extracted_json:
-#         #     raise HTTPException(status_code=500, detail="Failed to extract data. JSON response is empty.")
-        
-#         # print("extracted_json:", extracted_json)
-#         json_dict = json.loads(extracted_json)
-#         return  json_dict
- 
-#     except Exception as e:
-#         print(f"Error: {e}")
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-
 from fastapi import FastAPI, File, UploadFile, HTTPException, Form, Security, Depends
 from fastapi.responses import JSONResponse
 from fastapi.security.api_key import APIKeyHeader
@@ -78,12 +41,6 @@ async def upload_file(
 
         # Extract JSON from document
         extracted_json = await cv_json(temp_file_path)
-
-        # Ensure JSON parsing is successful
-        # try:
-        #     json_dict = json.loads(extracted_json)
-        # except json.JSONDecodeError:
-        #     raise HTTPException(status_code=500, detail="Failed to parse JSON response.")
 
         return extracted_json
 
