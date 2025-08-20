43d6d5373e9e93a1b2a6b48325828ce57aba3179
TreesWise <140375595+TreesWise@users.noreply.github.com>
2025-08-20 17:01:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1269,7 +1269,7 @@ def export_data_to_excel():
                 """))
                 print("[TASK] Updated rows to 'exported'", flush=True)
                 # conn2.execute(text("DELETE FROM temp_table WHERE TRIM(status) = 'exported' AND DATE(CreatedDate) < DATE('now')"))
-                conn2.execute(text("DELETE FROM temp_table WHERE TRIM(status) = 'exported'")
+                conn2.execute(text("DELETE FROM temp_table WHERE TRIM(status) = 'exported'"))
                 print("[TASK] Deleted exported rows", flush=True)
 
     except Exception as e:
@@ -1424,7 +1424,7 @@ def start_scheduler_guarded():
                 run_both_tasks,
                 CronTrigger(
                     hour=17,             # Current hour
-                    minute=10,           # Current minute
+                    minute=15,           # Current minute
                     timezone=SCHED_TZ    # Correct timezone (Asia/Kolkata)
                 ),
                 id="run_both_tasks_now",   # Change the ID to reflect immediate execution
@@ -1456,6 +1456,7 @@ async def shutdown_scheduler():
         print("[SCHEDULER] APScheduler stopped", flush=True)
     except Exception:
         pass
+
 
 
 
